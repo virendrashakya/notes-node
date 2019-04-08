e3d08266ba9691267d2eb8ee309ee492209df1ea
Virendra Shakya <virendrashakya.knp@gmail.com>
2019-04-09 00:00:47 +0530

    code cleanup

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
+//node 3rd party module
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
+//imported user files
 const notes = require('./notes.js');
-const user = os.userInfo();
-//  console.trace(user.username);
-
-try {
-  fs.appendFileSync('message.cred', `\nHello ${user.username}`);
-} catch (err) {
-  /* Handle the error */
-}
-
-let add_result = notes.add(9,-2);
-console.log('added result => ', add_result);
--- notes.js
+++ notes.js
@@ -1,3 +1,3 @@
-module.exports.add = (a,b) => {
-  return a + b ;
-}
+module.export.addNote = () => {
+  return 'New Note';
+};
