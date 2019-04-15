56df4117e978c8580307bf632fc732b45241aa26
Virendra Shakya <virendrashakya.knp@gmail.com>
2019-04-15 23:16:14 +0530

    completed notes app

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 //node 3rd party module
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 //imported user files
 const notes = require('./notes.js');
 
 let command = process.argv[2];
+let argv = yargs.argv;
+console.log(command);
 
 if (command === 'add') {
-  console.log('adding new note...');
+  let result = notes.addNote(argv.title, argv.body);
+  if (result.success) {
+    console.log('\x1b[32m', result.message);
+    console.log('\x1b[0m', '');
+  } else {
+    console.log('\x1b[31m', result.message);
+    console.log('\x1b[0m', '');
+  }
 } else if (command === 'list')  {
-  console.log('listing all note...');
+  let result = notes.getAllNotes();
+  console.log(`Total ${result.length} notes found`);
+  result.forEach(note => {
+    console.log('-----------------------------------')
+    console.log('Title: ', note.title);
+    console.log('Title: ', note.body);
+    console.log('-----------------------------------')
+  });
 } else if (command === 'read') {
-  console.log('reading note...', process.argv[3]);
+  let result = notes.readNotes(argv.title);
+  if (result.success) {
+    console.log('\x1b[32m', result.message);
+    console.log('\x1b[0m', '');
+  } else {
+    console.log('\x1b[31m', result.message);
+    console.log('\x1b[0m', '');
+  }
 } else if (command === 'remove') {
-  console.log('removing note...');
+  let result = notes.removeNotes(argv.title);
+  if (result.success) {
+    console.log('\x1b[32m', result.message);
+    console.log('\x1b[0m', '');
+  } else {
+    console.log('\x1b[31m', result.message);
+    console.log('\x1b[0m', '');
+  }
 } else {
   console.log('command not recognized !');
 }
--- notes.js
+++ notes.js
@@ -1,3 +1,89 @@
-module.exports.addNote = () => {
-  return 'New Note';
-};
+const fs = require('fs');
+const notes_db = 'notes-db.json';
+
+let fetchNotes = function () {
+  try {
+    let fetched_notes = fs.readFileSync(notes_db);
+    return JSON.parse(fetched_notes);
+  } catch (exception) {
+    return [];
+  }
+}
+
+let saveNotes = function (notes) {
+  fs.writeFileSync(notes_db, JSON.stringify(notes));
+}
+
+let addNote = (title, body) => {
+  let notes = fetchNotes();
+  let note = {
+    title,
+    body
+  };
+
+  let duplicatedNotes = notes.filter((note) => note.title === title);
+
+  if (duplicatedNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return {
+      success: true,
+      message: 'note created sucessfully',
+      title,
+      body
+    };
+  } else {
+    return {
+      success: false,
+      message: 'A note with similar title exists',
+      title,
+      body
+    };
+    // console.log('\x1b[41m', `There is already a note exists with title -> ${title}`);
+    // console.log('\x1b[0m', '');
+  }
+}
+
+let getAllNotes = () => {
+  return fetchNotes();
+}
+
+let readNotes = (title) => {
+  let notes = fetchNotes();
+  let readNote = notes.filter((note) => note.title === title);
+  if (readNote.length === 0) {
+    return {
+      success: false,
+      message: `Not note exist for title: ${title}`
+    }
+  } else {
+    return {
+      success: true,
+      message: `title: ${title} \n Body: ${readNote[0].body}`
+    }
+  }
+}
+
+let removeNotes = (title) => {
+  let notes = fetchNotes();
+  let deleteNote = notes.filter((note) => note.title !== title);
+  if (notes.length > deleteNote.length) {
+    saveNotes(deleteNote);
+    return {
+      success: true,
+      message: `Note removed for title: ${title}`
+    }
+  } else {
+    return {
+      success: false,
+      message: `Not note exist for title: ${title}`
+    }
+  }
+}
+
+module.exports = {
+  addNote,
+  getAllNotes,
+  readNotes,
+  removeNotes
+}
